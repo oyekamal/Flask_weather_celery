ffa0adecd576c73c9208578b63bd1ef08bdeac84
kamalpay <muhammadkamal@paymob.com>
2022-12-10 23:01:31 +0500

    crud operstion done

--- flask-on-docker/services/web/project/__init__.py
+++ flask-on-docker/services/web/project/__init__.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
+ma = Marshmallow(app)
 db = SQLAlchemy(app)
-
 
 class Subscription(db.Model):
     __tablename__ = "subscription"
@@ -16,6 +19,56 @@ class Subscription(db.Model):
     def __init__(self, city):
         self.city = city
 
+class SubscriptionSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Subscription
+        fields = ('city', 'active')
+
+
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+
+@app.route('/api/sub/', methods=['Post'])
+def subs_create():
+    city = request.json['city']
+    sub = Subscription(city)
+    sub_schema = SubscriptionSchema()
+    db.session.add(sub)
+    db.session.commit()
+    return jsonify(sub_schema.dump(sub))
+
+
+@app.route('/api/sub/', methods=['GET'])
+def subs():
+    sub = Subscription.query.filter()
+    subs_schema=SubscriptionSchema(many=True)
+    return jsonify(subs_schema.dump(sub))
+
+
+@app.route('/api/sub/<int:sub_id>/', methods=['GET'])
+def sub_detail(sub_id):
+    note = Subscription.query.get(sub_id)
+    sub_schema=SubscriptionSchema()
+    return sub_schema.jsonify(note)
+
+
+@app.route('/api/sub/<int:sub_id>/', methods=['PATCH'])
+def update_note(sub_id):
+    sub = Subscription.query.get(sub_id)
+    sub_schema=SubscriptionSchema()
+    city = request.json.get("city")
+    sub.city = city
+    db.session.add(sub)
+    db.session.commit()
+    return sub_schema.jsonify(sub)
+
+
+@app.route('/api/sub/<int:sub_id>/', methods=['DELETE'])
+def delete_note(sub_id):
+    sub = Subscription.query.get(sub_id)
+    sub_schema=SubscriptionSchema()
+    db.session.delete(sub)
+    db.session.commit()
+    return sub_schema.jsonify(sub)
